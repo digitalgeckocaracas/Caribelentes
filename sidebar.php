f443f12c1659890a6d338ee2f2b1fe6153250e38
digitalgeckocaracas <30294730+digitalgeckocaracas@users.noreply.github.com>
2017-07-19 12:00:36 -0400

    Delete sidebar.php

--- sidebar.php
+++
@@ -1,7 +0,0 @@
-<aside itemscope itemprop="http://www.schema.org/WPSideBar" role="complementary">
-	<?php if ( is_active_sidebar( 'main-sidebar' ) ) { ?>
-		<div id="widget-area" class="widget-area" role="complementary">
-			<?php dynamic_sidebar( 'main-sidebar' ); ?>
-		</div><!-- .widget-area -->
-	<?php } ?>
-</aside>
